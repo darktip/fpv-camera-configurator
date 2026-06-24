79d6e3ee8482a9742f38232b6d6f46ac028c8f10
Volodymyr Melnychuk <darktipzero@gmail.com>
2026-06-24 11:31:59 +0300

    Add code to test potentiometer values via serial input

--- src/main.cpp
+++ src/main.cpp
@@ -4,66 +4,119 @@ const int INC = 18;
 const int UD  = 17;
 const int CS  = 16;
 
-void pulse() {
+int g_currentPosition = 0;
+
+void pulseInc() 
+{
     digitalWrite(INC, LOW);
-    delayMicroseconds(50);
+    delay(2);
     digitalWrite(INC, HIGH);
-    delayMicroseconds(50);
+    delay(2);
 }
 
-void moveToZero() {
+void csExistWithoutEepromSaving()
+{
+    digitalWrite(INC, LOW);
+    delayMicroseconds(2);
+    digitalWrite(CS, HIGH);
+    digitalWrite(INC, HIGH);
+}
+
+void moveToZero() 
+{
     digitalWrite(CS, LOW);
     digitalWrite(UD, LOW);
+    delayMicroseconds(2);
 
     for (int i = 0; i < 120; i++) {
-        pulse();
+        pulseInc();
     }
 
-    digitalWrite(CS, HIGH);
+    csExistWithoutEepromSaving();
+
+    g_currentPosition = 0;
 }
 
-void moveTo50() {
-    digitalWrite(CS, LOW);
-    digitalWrite(UD, HIGH);
-
-    for (int i = 0; i < 50; i++) {
-        pulse();
+void moveToTarget(int targetPosition) 
+{
+    if (targetPosition == g_currentPosition) 
+    {
+        return;
     }
 
-    digitalWrite(CS, HIGH);
-}
+    if (targetPosition < 0) 
+    {
+      targetPosition = 0;
+    }
+    else if (targetPosition > 99) 
+    {
+      targetPosition = 99;
+    }
 
-void moveTo99() {
     digitalWrite(CS, LOW);
-    digitalWrite(UD, HIGH);
+    int increments = abs(targetPosition - g_currentPosition);
+    int direction = (targetPosition > g_currentPosition) ? HIGH : LOW;
+    digitalWrite(UD, direction);
+    Serial.printf("Current=%d Target=%d Steps=%d\n", g_currentPosition, targetPosition, increments);
+    delayMicroseconds(2);
 
-    for (int i = 0; i < 120; i++)
-        pulse();
+    for (int i = 0; i < increments; i++) 
+    {
+        pulseInc();
+    }
 
-    digitalWrite(CS, HIGH);
+    csExistWithoutEepromSaving();
+    g_currentPosition = targetPosition;
 }
 
-void setup() {
+void setupPins() 
+{
     pinMode(LED_BUILTIN, OUTPUT);
-    pinMode(INC, OUTPUT);
-    pinMode(UD, OUTPUT);
-    pinMode(CS, OUTPUT);
 
-    // 1. Switch to RED (Red=255, Green=0, Blue=0)
-    neopixelWrite(RGB_BUILTIN, 255, 0, 0);
     digitalWrite(INC, HIGH);
+    digitalWrite(CS, HIGH);
+    digitalWrite(UD, LOW);
+    
+    pinMode(INC, OUTPUT);
+    pinMode(CS, OUTPUT);
+    pinMode(UD, OUTPUT);
+}
 
+void setupDefalts() 
+{
+    neopixelWrite(RGB_BUILTIN, 255, 0, 0);
+}
+
+void setup() 
+{
+    Serial.begin(115200);
+    setupPins();
+    setupDefalts();
     moveToZero();
 }
 
-void loop() {
-    moveToZero();
-    // 2. Switch to GREEN (Red=0, Green=255, Blue=0)
-    neopixelWrite(RGB_BUILTIN, 0, 255, 0);
-    delay(5000);
-
-    moveTo99();
-    // 3. Switch to BLUE (Red=0, Green=0, Blue=255)
-    neopixelWrite(RGB_BUILTIN, 0, 0, 255);
-    delay(5000);
+void loop() 
+{
+    if (Serial.available()) 
+    {
+        String input = Serial.readStringUntil('\n');
+        int targetPosition = input.toInt();
+        
+        // if (targetPosition == 0)
+        // {
+        //     moveToZero();
+        // }
+        // else if (targetPosition > 0 && targetPosition <= 99) 
+        if (targetPosition >= 0 && targetPosition <= 99)
+        {
+            Serial.println("Moving to position: " + String(targetPosition));
+            moveToTarget(targetPosition);
+        } 
+        else 
+        {
+            Serial.println("Invalid position! Enter 0-99");
+        }
+    }
+    
+    delay(100);
 }
